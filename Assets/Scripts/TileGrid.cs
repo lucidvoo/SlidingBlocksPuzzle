cd30406d6418a9ed594d81df36a08369bbcc8618
lucidvoo <90751594+lucidvoo@users.noreply.github.com>
2022-03-28 15:24:50 +0500

    grid array initialization and event system added

--- Assets/Scripts/TileGrid.cs
+++ Assets/Scripts/TileGrid.cs
@@ -6,5 +6,37 @@ using UnityEngine;
 
 public class TileGrid : MonoBehaviour
 {
-    
+    [Tooltip("Add all tiles in any order")]
+    [SerializeField] private Tile[] tileRefs;
+
+    private Tile[,] tiles;
+
+    private void Awake()
+    {
+        InitializeTileArray();
+    }
+
+    // fill 2-dim tiles array with refs from editor
+    private void InitializeTileArray()
+    {
+        if (tileRefs.Length < 4)
+        {
+            Debug.LogError("Fill tileRefs array in tile grid!");
+        }
+        
+        int arraySide = Mathf.RoundToInt(Mathf.Sqrt(tileRefs.Length));
+        tiles = new Tile[arraySide, arraySide];
+
+        for (int i = 0; i < tileRefs.Length; i++)
+        {
+            tiles[tileRefs[i].CorrectTileIndexes.x, tileRefs[i].CorrectTileIndexes.y] = tileRefs[i];
+        }
+
+        //Test
+        Debug.Log("tiles in order:");
+        foreach (var item in tiles)
+        {
+            Debug.Log(item.CorrectTileIndexes.x + ", " + item.CorrectTileIndexes.y);
+        }
+    }
 }
